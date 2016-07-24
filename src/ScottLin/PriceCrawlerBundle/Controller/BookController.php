f0b74e0844d25329904f26c50fd25f3614396880
Maja_no2 <typhoon31815@gmail.com>
2016-07-25 02:53:57 +0800

    Now all discount info will be saved into redis. Under the same route(/book/discount/week/{source}) use PUT to update the data in redis, and GET to get data from redis.

--- src/ScottLin/PriceCrawlerBundle/Controller/BookController.php
+++ src/ScottLin/PriceCrawlerBundle/Controller/BookController.php
@@ -8,6 +8,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Predis\Autoloader;
+use Predis\Client;
 use MichaelSchrenk\WebbotBundle\Library\Parse;
 use MichaelSchrenk\WebbotBundle\Library\Http;
 use ScottLin\PriceCrawlerBundle\Library\WeekDiscount;
@@ -23,17 +25,16 @@ class BookController extends Controller
     }
 
     /**
-     * Get weekly discount book infomation. Due to the UTF-8 json problem,
-     * reciver must remember to urldecode & unserialize the data.
+     * Get weekly discount book infomation and save it to redis.
      *
      * @Route(
      *      "/book/discount/week/{source}",
-     *      name="discount_week",
+     *      name="discount_week_update",
      *      requirements={"source" : "bookscom|taaze|sanmin|kingstone|iread"})
      *
-     * @Method("GET")
+     * @Method("PUT")
      */
-    public function weeklyDiscountAction(Request $request, $source)
+    public function weeklyDiscountUpdateAction(Request $request, $source)
     {
         $parse = new Parse;
 
@@ -147,6 +148,10 @@ class BookController extends Controller
         ];
 
         try {
+
+            Autoloader::register();
+            $redis = new Client();
+
             $page = new WeekDiscount($link[$source], $ref, "GET", "", $source);
             $parsingResult = $page->targetPageParsing();
 
@@ -163,10 +168,12 @@ class BookController extends Controller
             }
 
             $page->returnBookInfo($parsingResult['FILE'], $source);
+            $finalPage = urlencode(serialize($page->getBookInfo()));
+            $redis->set($source, json_encode($finalPage));
 
             $result = [
                 'status' => 'successful',
-                'data' => urlencode(serialize($page->getBookInfo())),
+                'data' => $finalPage,
             ];
 
         } catch (\Exception $e) {
@@ -180,6 +187,55 @@ class BookController extends Controller
         }
         return new JsonResponse($result);
     }
+
+    /**
+     * Get weekly discount book infomation from redis.
+     *
+     * @Route(
+     *      "/book/discount/week/{source}",
+     *      name="discount_week",
+     *      requirements={"source" : "bookscom|taaze|sanmin|kingstone|iread"})
+     *
+     * @Method("GET")
+     */
+    public function weeklyDiscountAction(Request $request, $source)
+    {
+        try {
+            Autoloader::register();
+            $redis = new Client();
+
+            $page = $redis->get($source);
+            if (empty($page) || is_null($page)) {
+                throw new \Exception("There is no data.", 100);
+            }
+            if (strlen($page) <= 200) {
+                throw new \Exception("Something wrong with data.", 101);
+            }
+
+            $result = [
+                'status' => 'successful',
+                'data' => json_decode($page),
+            ];
+
+        } catch (\Exception $e) {
+            if ($e->getCode() == 100 || $e->getCode() == 101) {
+                $response = $this->forward(
+                    'PriceCrawlerBundle:Book:weeklyDiscountUpdate',
+                    ['source' => $source]
+                );
+                return $response;
+            }
+            $result = [
+                'status' => 'failed',
+                "error" => [
+                    'message' => $e->getMessage(),
+                    'code' => $e->getCode(),
+                ]
+            ];
+        }
+        return new JsonResponse($result);
+    }
+
 
     /**
      * @Route("/book/discount/bookstore", name="discount_beginookstore")
